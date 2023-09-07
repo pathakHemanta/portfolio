e6940548cb97df82f5fc3e0966ff4b30657af5ae
pathakhHemanta <pathakh@warhawks.ulm.edu>
2023-09-06 22:22:00 -0700

    Last of Api Routes

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method == "POST") {
+    // Some action related to POST
+  } else {
+    //Handle any other HTTP method
+  }
+  res.status(200).json({ name: "John Doe" });
 }
